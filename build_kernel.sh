83663ea9a92879ab380a67ffe4191383e7d40fe0
physwizz <pollockg86@gmail.com>
2025-08-04 15:33:45 +1000

    fix error

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,6 +9,6 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y gta7litewifi_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
--- fs/exec.c
+++ fs/exec.c
@@ -1906,7 +1906,8 @@ extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);+#endif
+				 void *argv, void *envp, int *flags);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
